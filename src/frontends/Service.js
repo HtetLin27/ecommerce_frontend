3fe3de4d6cc9bc34eb3210bee886cfde38486f99
HtetLin27 <105052327+HtetLin27@users.noreply.github.com>
2022-07-13 20:39:53 +0630

    node-module commit

--- src/frontends/Service.js
+++ src/frontends/Service.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+const axios = require('axios')
 
 function Service(props){
 
+const [data,setData] =useState([]);
+   axios.get("http://127.0.0.1:8000/services").then(response=>{
+    setData(response.data)
+   })
 
     
     return(
         <section className="container">
             <h2 className="service-title text-center mt-3">Our Service</h2>
             <div className="row mt-5">
-                <div className="col-md-4">
-                    <img src="/assets/image/resizesocial1.png" alt="social"/>
-                    <h2 className="mt-3">Digital Marketing</h2>
-                    <p className="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                </div>
+              
+                {
+                    data.map(e=>{
+                        return(
+                            <div className="col-md-4">
+                            {/* <img src={"http://127.0.0.1:8000/images/"+e.image} alt="social"/> */}
+                            <h2 className="mt-3">{e.title}</h2>
+                            <p className="text-justify">{e.description}</p>
+                            </div>
+                        ) }
+                    ) 
+                 }
                 
             </div>
         </section>
